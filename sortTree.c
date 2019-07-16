8a3112558f0b0e86efa8ffe961342b634513cea3
010DaweiZhou <40254594+010DaweiZhou@users.noreply.github.com>
2019-07-16 18:57:37 +0800

    add delete node
    add delete node

--- sortTree.c
+++ sortTree.c
@@ -17,12 +17,25 @@ typedef struct{
 	Node * root;
 }Tree;
 
-void insert (Tree * tree , int value)
+/*
+	insert the small one in left ,
+	insert the big one in right,
+	no operation if value exist.
+*/
+void insert (Tree * tree , int value , Node * node)
 {
-	Node * node = malloc(sizeof(Node));
-	malloc_total += sizeof(Node);
-	memset (node , 0 , sizeof(Node));
-	node->data = value;
+	if(node == NULL)
+	{
+		node = malloc(sizeof(Node));
+		malloc_total += sizeof(Node);
+		memset (node , 0 , sizeof(Node));
+		node->data = value;
+	}
+	else
+	{
+		node->left = NULL;
+		node->right = NULL;
+	}
 	
 	if(tree->root == NULL)
 	{	
@@ -43,7 +56,7 @@ void insert (Tree * tree , int value)
 				else
 					temp = temp->left;
 			}
-			else
+			else if(value > temp->data)
 			{
 				if(temp->right == NULL)
 				{
@@ -55,11 +68,135 @@ void insert (Tree * tree , int value)
 					temp = temp->right;
 				}
 			}
+			else
+			{
+				free(node);
+				free_total += sizeof(Node);
+				return;
+			}
 		}
 	}
 }
 
-void freeTreeNode(Node * node)
+void insert_after_delete_node(Tree * tree, Node * node)
+{
+	if(tree == NULL || node == NULL)
+		return;
+	
+	insert_after_delete_node(tree, node->left);
+	insert_after_delete_node(tree, node->right);
+	insert(tree, node->data , node);
+}
+
+
+/* delete the node which is specified */
+void deleteNode( Tree * tree , int value)
+{
+	Node * node = tree->root;
+	Node * father_node = node;
+	
+	while(node != NULL )
+	{
+		/*left is small*/
+		if(value > node->data)
+		{
+			father_node = node;
+			node = node->right;
+		}
+		/* right is big */
+		else if(value < node->data)
+		{
+			father_node = node;
+			node = node->left;
+		}
+		/* equal */
+		else
+		{
+			Node * left_node = node->left;
+			Node * right_node = node->right;
+			
+			/* no left node and right node , delete directly */
+			if(!left_node && !right_node)
+			{
+				father_node->left == node ? (father_node->left = NULL) : (father_node->right = NULL);
+				free(node);
+				node = NULL;
+				free_total += sizeof(Node);
+			}	
+			/* no left but right existed , copy right to this node , free the right */
+			else if(!left_node && right_node)
+			{
+				memcpy(node , right_node ,sizeof(Node));
+				free(right_node);
+				node = NULL;
+				free_total += sizeof(Node);
+			}
+			
+			else if(left_node && !right_node)
+			{
+				memcpy(node , left_node ,sizeof(Node));
+				free(left_node);
+				node = NULL;
+				free_total += sizeof(Node);
+			}
+			
+			/* both existed */
+			else
+			{
+				/*root node ,add left node to right */
+				if(node == tree->root)
+				{
+					tree->root = right_node;
+					
+					free(node);
+					node = NULL;
+					free_total += sizeof(Node);
+				
+					insert_after_delete_node(tree,left_node);
+					return;
+				}
+				/* no root node , move left and right to the tree */
+				father_node->left == node ? (father_node->left = NULL) : (father_node->right = NULL);
+				free(node);
+				node = NULL;
+				free_total += sizeof(Node);
+				
+				insert_after_delete_node(tree,left_node);
+				insert_after_delete_node(tree,right_node);
+			}
+			/*
+			if(node == tree->root)
+			{
+				Node * left_node = node->left;
+				Node * right_node = node->right;
+				tree->root = right_node;
+				
+				free(node);
+				node = NULL;
+				free_total += sizeof(Node);
+				
+				insert_after_delete_node(tree,left_node);
+			}
+			else
+			{
+				Node * left_node = node->left;
+				Node * right_node = node->right;
+
+				father_node->left == node ? (father_node->left = NULL) : (father_node->right = NULL);
+				free(node);
+				node = NULL;
+				free_total += sizeof(Node);
+					
+				insert_after_delete_node(tree,left_node);
+				insert_after_delete_node(tree,right_node);
+			}
+			*/
+		}
+	}
+}
+
+/* free all node on the tree */
+void freeALLTreeNode(Node * node)
 {
 	if(node == NULL)
 		return;
@@ -71,10 +208,11 @@ void freeTreeNode(Node * node)
 	free_total += sizeof(Node);
 	node = NULL;
 	
-	freeTreeNode(left);
-	freeTreeNode(right);
+	freeALLTreeNode(left);
+	freeALLTreeNode(right);
 }
 
+/* scan the tree */
 void inoder(Node * node)
 {
 	if(node == NULL)
@@ -85,33 +223,66 @@ void inoder(Node * node)
 	inoder(node->right);
 }
 
+
+/* get the tree depth */
+int depth(Node * node)
+{
+	if(node == NULL)
+		return 0;
+	
+	int leftmax = depth(node->left);
+	int rightmax = depth(node->right);
+	
+	if(leftmax > rightmax)
+		return leftmax + 1;
+	else
+		return rightmax + 1;
+	
+}
+
 int main(int argc , char ** argv)
 {
 	Tree * tree = malloc(sizeof(Tree));
 	malloc_total += sizeof(Tree);
 	memset(tree , 0 ,sizeof(Tree));
 	
-	for(int i = 0; i < 10 ; i ++ )
+	for(int i = 0; i < 15 ; i ++ )
 	{
-		insert(tree,i);
+		insert(tree,i,NULL);
 	}
 	
 	for(int i = -10; i < 0 ; i ++ )
 	{
-		insert(tree,i);
+		insert(tree,i,NULL);
 	}
 	
-	insert(tree , -11);
-	insert(tree , 5);
-	insert(tree , 5);
+	insert(tree , -11,NULL);
+	//insert(tree , 5,NULL);
+	//insert(tree , 5,NULL);
+	
 	
 	inoder(tree->root);
 	printf("\n");
+	printf("depth of tree = %d\n",depth(tree->root));
 	
-	freeTreeNode(tree->root);
+	
+	deleteNode(tree,0);
+	inoder(tree->root);
+	printf("\n");
+	printf("depth of tree = %d\n",depth(tree->root));
+	
+	//deleteNode(tree,1);
+	//deleteNode(tree,2);
+
+	deleteNode(tree,-10);
+	inoder(tree->root);
+	printf("\n");
+	printf("depth of tree = %d\n",depth(tree->root));
+	
+	
+	freeALLTreeNode(tree->root);
 	free(tree);
 	free_total += sizeof(Tree);
-	
 	printf("malloc total = %d , free total = %d\n",malloc_total , free_total);
 	exit(0);
 }
